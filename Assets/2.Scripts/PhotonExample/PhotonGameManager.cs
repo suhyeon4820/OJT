2add8a33560777628883d5b6e896cacf72b6740e
suhyeon4820 <bananacco@naver.com>
2023-03-19 22:36:43 +0900

    03 게임씬

---
+++ Assets/2.Scripts/PhotonExample/PhotonGameManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.SceneManagement;
+public class PhotonGameManager : MonoBehaviourPunCallbacks
+{
+    public override void OnLeftRoom()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void LeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+    }
+}
