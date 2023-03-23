f2694efa089e15791717dfd5d611b0c338602482
suhyeon <suhyeon@salin.co.kr>
2023-03-23 16:23:44 +0900

    채팅 구현

--- Assets/2.Scripts/PhotonChat/PhotonChatManager.cs
+++ Assets/2.Scripts/PhotonChat/PhotonChatManager.cs
@@ -4,66 +4,102 @@ using UnityEngine;
 using Photon.Chat;
 using Photon.Pun;
 using ExitGames.Client.Photon;
-
+using System;
 public class PhotonChatManager : MonoBehaviour, IChatClientListener
 {
-    ChatClient chatClinet;
+    public Action<string, string> ReturnChatInputAction;
+
+    protected internal ChatAppSettings chatAppSettings;
+
+    ChatClient chatClient;
     [SerializeField] string userID;
-    bool isConnected;
+    
     string chatChannelName = "OJT";
 
+ 
     private void Start()
     {
-        ChatConnect();  
+        DontDestroyOnLoad(this.gameObject);
+        
+        Connect();
+
+
     }
 
-    public void ChatConnect()
+    private void OnApplicationQuit()
     {
-        isConnected = true;
+        if(chatClient!=null)
+        {
+            chatClient.Disconnect();
+        }
+    }
 
-        chatClinet = new ChatClient(this);
-
+    public void Connect()
+    {
+        chatClient = new ChatClient(this);
         string appID = PhotonNetwork.PhotonServerSettings.AppSettings.AppIdChat;
         string appVersion = PhotonNetwork.AppVersion;
         AuthenticationValues authValues = new AuthenticationValues(PhotonNetwork.NickName);
-        chatClinet.Connect(appID, appVersion, authValues);
+        chatClient.Connect(appID, appVersion, authValues);
+
     }
+
+ 
     private void Update()
     {
         // 필요한가?
-        if(isConnected)
+        if(chatClient!=null)
         {
-            chatClinet.Service();
+            chatClient.Service();
         }
-       
     }
 
+    #region 메세지 보내기
+    public void SendInputMessage(string message)
+    {
+        chatClient.PublishMessage(chatChannelName, message);
+    }
+    #endregion
+
+    #region photonchat 연결 및 구독
     public void DebugReturn(DebugLevel level, string message)
     {
-        throw new System.NotImplementedException();
+        if (level == DebugLevel.ERROR)
+        {
+            Debug.LogError(message);
+        }
+        else if (level == DebugLevel.WARNING)
+        {
+            Debug.LogWarning(message);
+        }
+        else
+        {
+            Debug.Log(message);
+        }
     }
 
     public void OnChatStateChange(ChatState state)
     {
-        throw new System.NotImplementedException();
+        Debug.Log(state); // 현재 클라이언트의 상태를 출력
     }
-
+    //  서버와 연결되면 호출
     public void OnConnected()
     {
-        Debug.Log("OnConnected");
-        // photonchat 서버와 연결
-        isConnected = true;
-        chatClinet.Subscribe(chatChannelName);
+        chatClient.Subscribe(chatChannelName); // 채널명으로 접속
     }
 
     public void OnDisconnected()
     {
-        throw new System.NotImplementedException();
+        Debug.Log("OnDisconnected");
     }
-
+    // Update() 의 chatClient.Service() 가 매 호출 시 OnGetMessages 를 호출
     public void OnGetMessages(string channelName, string[] senders, object[] messages)
     {
-        throw new System.NotImplementedException();
+        if(channelName == chatChannelName)
+        {
+            Debug.Log(channelName + senders[0] + messages[0]);
+            ReturnChatInputAction?.Invoke(senders[0].ToString(), messages[0].ToString());
+        }
     }
 
     public void OnPrivateMessage(string sender, object message, string channelName)
@@ -78,12 +114,14 @@ public class PhotonChatManager : MonoBehaviour, IChatClientListener
 
     public void OnSubscribed(string[] channels, bool[] results)
     {
-        throw new System.NotImplementedException();
+        Debug.Log(string.Format("채널 입장 {0} : {1}", channels[0], results[0]));
+        //throw new System.NotImplementedException();
     }
 
     public void OnUnsubscribed(string[] channels)
     {
-        throw new System.NotImplementedException();
+        Debug.Log(string.Format("채널 퇴장 {0}", channels));
+        //throw new System.NotImplementedException();
     }
 
     public void OnUserSubscribed(string channel, string user)
@@ -95,4 +133,5 @@ public class PhotonChatManager : MonoBehaviour, IChatClientListener
     {
         throw new System.NotImplementedException();
     }
+    #endregion
 }
--- Assets/2.Scripts/PhotonChat/UIChatting.cs
+++ Assets/2.Scripts/PhotonChat/UIChatting.cs
@@ -6,11 +6,17 @@ using TMPro;
 public class UIChatting : MonoBehaviour
 {
     [SerializeField] TMP_InputField inputField;
+    [SerializeField] TMP_Text chatText;
     [SerializeField] ScrollRect scrollRect;
     [SerializeField] PhotonChatManager photonChatManager;
     private void Start()
     {
         SetListener();
+        photonChatManager.ReturnChatInputAction += UpdateChattingWindow;
+    }
+    private void OnDisable()
+    {
+        photonChatManager.ReturnChatInputAction -= UpdateChattingWindow;
     }
 
     void SetListener()
@@ -20,8 +26,21 @@ public class UIChatting : MonoBehaviour
 
     void GetUserInput(string userInput)
     {
-        Debug.Log(userInput);
-        photonChatManager.SendMessage(userInput);
+        if(string.IsNullOrEmpty(userInput))
+        {
+            return;
+        }
+        else
+        {
+            Debug.Log(userInput);
+            photonChatManager.SendInputMessage(userInput);
+        }   
         inputField.text = "";
+    }
+
+    void UpdateChattingWindow(string userName, string inputMessage)
+    {
+        string message = string.Format("[{0}] : {1}\n", userName, inputMessage);
+        chatText.text += message;
     }
 }
